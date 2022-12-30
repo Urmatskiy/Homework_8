3c965c66406630873600c1ba3cdf0948ea6a3b3f
Urmatskiy <nickiton14@gmail.com>
2022-12-30 09:24:54 +0300

    Выполнил 58 задание

---
+++ task_58/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+
+int[,] CreateMassive(int row, int column, int start, int end)
+{
+    int[,] arr = new int[row, column];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            arr[i, j] = new Random().Next(start, end + 1);
+        }
+    }
+    return arr;
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ProductOfArrays(int[,] arr1, int[,] arr2)
+{
+    int[,] newArray = new int[2, 2];
+    newArray[0, 0] = arr1[0, 0] * arr2[0, 0] + arr1[0, 1] * arr2[1, 0];
+    newArray[0, 1] = arr1[0, 0] * arr2[0, 1] + arr1[0, 1] * arr2[1, 1];
+    newArray[1, 0] = arr1[1, 0] * arr2[0, 0] + arr1[1, 1] * arr2[1, 0];
+    newArray[1, 1] = arr1[1, 0] * arr2[0, 1] + arr1[1, 1] * arr2[1, 1];
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            Console.Write($"{newArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int rows = 2;
+int columns = 2;
+int[,] firstArray = CreateMassive(rows, columns, 1, 9);
+int[,] secondArray = CreateMassive(rows, columns, 1, 9);
+PrintArray(firstArray);
+Console.WriteLine();
+PrintArray(secondArray);
+Console.WriteLine();
+ProductOfArrays(firstArray, secondArray);
